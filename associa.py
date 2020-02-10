9c3fb00f10fae8c425a1034ce217c25ba47fa864
Kritsana Manupha <kritsana.mnp@gmail.com>
2020-02-10 16:35:28 +0700

    นับจำนวน Product แต่ละ Transactionได้ | หา Supmin ได้

--- associa.py
+++ associa.py
@@ -1,5 +1,7 @@
 import random
 from collections import Counter
+import operator
+import math 
 #  PRODUCT NAME
 product = ["Apple", "Banana", "Coke", "Icesream", "Sprice", "Knife", "Pan", "Pork", "Yogurt", "Tomato sauce",
 "Spaghetti", "Steamed bun", "Mama", "Orange juice", "Sandwich", "Water", "Sausage", "Beer", "Spy", "M150",
@@ -8,7 +10,7 @@ product = ["Apple", "Banana", "Coke", "Icesream", "Sprice", "Knife", "Pan", "Por
 "Toy", "Metal", "Wood", "Glass", "Battery", "Lay", "Vegetable", "Gun", "Smart watch", "Dumbell"]
 
 # GENERATE TRANSACTIONS
-transacNO = [None]*10
+transacNO = [None]*1000
 
 buyAmount1 = [None]*random.randrange(2, 6)
 buyAmount2 = [None]*random.randrange(6, 9)
@@ -20,7 +22,7 @@ maxProduct = 0
 eightypct=0
 twentypct=0
 
-while maxProduct < 10:
+while maxProduct < len(transacNO):
     instType = random.randrange(2)
 
     def createInsType1():
@@ -39,11 +41,11 @@ while maxProduct < 10:
             i += 1
         return inst1
 
-    if (instType == 0 and eightypct < 8):
+    if (instType == 0 and eightypct < 0.8*len(transacNO)):
         transacNO[maxProduct] = createInsType1()        
         eightypct += 1
         maxProduct += 1
-    if (instType == 1 and twentypct < 2):
+    if (instType == 1 and twentypct < 0.2*len(transacNO)):
         transacNO[maxProduct] = createInsType2()        
         twentypct += 1
         maxProduct += 1
@@ -51,20 +53,28 @@ while maxProduct < 10:
 
 # ASSOCIATION RULE
 
-minSup = 0.6
+minSup = math.floor(0.6*len(transacNO)/math.log(len(transacNO)))
 minCon = 0.8
 line = 0
 results = {}
+realResults = {}
+data = []
 
-for line in range(10):
+for line in range(len(transacNO)):
     globals()["myset" + str(line)] = set(transacNO[line])
-    globals()["data" + str(line)] = []
-    globals()["data" + str(line)] += globals()["myset" + str(line)]
+    data.append(globals()["myset" + str(line)])
 
+counts = dict()
 
-for i in data0:
-        results[i] = data1.count(i)+1
-for i in data1:
-        results[i] = data0.count(i)+1     
-print(results)
+for product in data:
+    for i in product:
+        counts[i] = counts.get(i, 0) + 1
+
+sorted_d = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
+print('(---------------------------------------------)')
+print('*** Sorted ***')
+print(sorted_d,'\n')
+print('       -----------------------------------')
+print('       | Min-support Transactions = ',minSup,' |')
+print('       -----------------------------------')
 
